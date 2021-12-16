983f3b95d61043734169f3eff8e830efed63b422
tankala <ashok_tankala@yahoo.com>
2021-12-16 20:27:43 +0530

    docker multi stage and simple library management web app

--- docker-multi-stage/app.py
+++ docker-multi-stage/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from random import randint
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<int(signed=True):number>")
+def get_random_number(number):
+    if number >= 0:
+        random_number = randint(0, number)
+        return f"Random number between 0 and {number}: {random_number}"
+    else:
+        random_number = randint(number, 0)
+        return f"Random number between {number} and 0: {random_number}"
---
+++ library-management-system/app.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "books.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.form:
+        book = Book(title=request.form.get("title"))
+        db.session.add(book)
+        db.session.commit()
+    books = Book.query.all()
+    return render_template("home.html", books=books)
+
+@app.route("/create_database", methods=["GET"])
+def create_database():
+    # db.create_all()
+    return "done"
